22ac5342488fc2ee60bab87712ade57f4ef5c67e
João Paulo Lethier <jplethier@gmail.com>
2015-04-25 14:58:41 -0300

    adds flash message to angular; adds validation and model tests to recipe;

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
   scope :by_name, lambda{ |keywords| where('name ilike ?',"%#{keywords}%") }
+
+  validates :name, presence: true
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Recipe do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipe) { FactoryGirl.build :recipe }
+
+  subject { recipe }
+
+  its(:valid?) { should be true }
+
+  describe 'validations' do
+    it 'requires a name' do
+      expect { recipe.name = nil }.to change{ recipe.valid? }.from(true).to(false)
+    end
+  end
 end
